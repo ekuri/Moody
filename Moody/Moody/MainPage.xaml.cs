153704a26c7694d75074699e137832a8cd7c4c02
ekuri <960956136@qq.com>
2015-05-14 02:58:01 +0800

    second commit

--- Moody/Moody/MainPage.xaml.cs
+++ Moody/Moody/MainPage.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+//using System.Threading;
+using System;
 
 // “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
 
@@ -34,10 +36,57 @@ namespace Moody
             new Mood("", "Assets/3d_funny.png"),
             new Mood("", "Assets/3d_funny.png"),
         };
+
+        Point moodBallCenter;
+        DispatcherTimer timer;
+
         public MainPage()
         {
             this.InitializeComponent();
             this.moodSelector.ItemsSource = this.allMood;
+            moodBall.Opacity = 0.5;
+            //moodBallCenter = moodBall.Margin.
         }
+
+        private void onUpperBall_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            timer.Tick += new EventHandler<object>(incTempValue);
+            timer.Start();
+        }
+
+
+
+        private void onLowerBall_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            timer.Tick += new EventHandler<object>(decTempValue);
+            timer.Start();
+        }
+
+        void incTempValue(object sender, object e)
+        {
+            if (temp.Value < 100)
+            {
+                temp.Value = temp.Value + 1;
+                moodBall.Opacity += 0.01;
+            }
+        }
+
+        void decTempValue(object sender, object e)
+        {
+            if (temp.Value > 0)
+                temp.Value = temp.Value - 1;
+        }
+
+        private void onUpperOrLowerBall_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            timer.Stop();
+        }
+
+
     }
 }
