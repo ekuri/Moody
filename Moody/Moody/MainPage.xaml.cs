5ae27d00cc9e4cc66600d88891408fb0182c9cd2
ekuri <960956136@qq.com>
2015-05-14 01:17:13 +0800

    add initial project file

---
+++ Moody/Moody/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
+
+namespace Moody
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        List<Mood> allMood = new List<Mood>() {
+            new Mood("Funny", "Assets/Mood/3d_funny.png"),
+            new Mood("Shock", "Assets/Mood/3d_shock.png"),
+            new Mood("A Little Sad", "Assets/Mood/a_little_sad.png"),
+            new Mood("", "Assets/3d_funny.png"),
+            new Mood("", "Assets/3d_funny.png"),
+            new Mood("", "Assets/3d_funny.png"),
+            new Mood("", "Assets/3d_funny.png"),
+            new Mood("", "Assets/3d_funny.png"),
+            new Mood("", "Assets/3d_funny.png"),
+            new Mood("", "Assets/3d_funny.png"),
+        };
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.moodSelector.ItemsSource = this.allMood;
+        }
+    }
+}
---
+++ Moody/Moody/Mood.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moody
+{
+    class Mood
+    {
+        public string text { set; get; }
+        public string src { set; get; }
+
+        public Mood(string text, string src)
+        {
+            this.text = text;
+            this.src = src;
+        }
+    }
+}
